7fa4338c874ae95d5aaa6e42e128088bb3e6e05a
huangyangyang <yangyang.huang@cloudfortdata.com>
2019-12-19 17:07:03 +0800

    添加python操作mysql

---
+++ Tools/python操作mysql/mysql/run/data/sql.py
@@ -0,0 +1,12 @@
+#-*- coding=utf-8 -*-
+_Auth_ = "yangyang.huang"
+
+#select
+sql_selectSystem = "select value from system_config where name = 'global.system.server.version';"
+
+#update
+sql_updateSystem = "update system_config set value = '1.9.5' where name = 'global.system.server.version';"
+
+#delete
+
+#insert into
