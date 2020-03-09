aee9bc6f29d1e5be89fa96d4f5e4cf3d9e4a5e74
huia <hyjxsj@163.com>
2020-03-09 22:48:53 +0800

    1.添加ignore文件 2.修改web_driver_wait2模板文件

--- 测试/自动化测试/UI自动化/custom-modules/web_driver_wait2/web_driver_wait2.py
+++ 测试/自动化测试/UI自动化/custom-modules/web_driver_wait2/web_driver_wait2.py
@@ -6,13 +6,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class WebDriverWait2(object):
-    def __init__(self,driver,timeout,ele_by_xpath):
+    def __init__(self,driver,ele_by_xpath=None):
         self.dr = driver
-        self.timeout = timeout
         self.ele_by_xpath = ele_by_xpath
 
-    def explicitly(self):
-        ele = WebDriverWait(self.dr, self.timeout).until(EC.presence_of_element_located((By.XPATH, self.ele_by_xpath)))
+    def explicitly(self,timeout):
+        ele = WebDriverWait(self.dr, timeout).until(EC.presence_of_element_located((By.XPATH, self.ele_by_xpath)))
         return ele
-    def implicitly(self):
-        self.dr.implicitly_wait(self.timeout)
+    def implicitly(self,timeout):
+        self.dr.implicitly_wait(timeout)
